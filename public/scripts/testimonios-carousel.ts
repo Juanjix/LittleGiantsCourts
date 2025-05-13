5cd236e2dcdb7d5aa6392264917e515ca4a36cc0
Juan José Peralta <juanjose.peralta92@gmail.com>
2025-05-12 23:27:39 -0300

    test / lo agregue afuera de la seccion

--- public/scripts/testimonios-carousel.ts
+++ public/scripts/testimonios-carousel.ts
@@ -1,15 +1,10 @@
-import EmblaCarousel from "embla-carousel";
+// @ts-ignore
+import EmblaCarousel from "https://cdn.skypack.dev/embla-carousel";
 
 export function initTestimoniosCarousel() {
-  const emblaNode = document.querySelector(
-    ".embla__viewport"
-  ) as HTMLElement | null;
-  const prevBtn = document.querySelector(
-    "#prevBtn"
-  ) as HTMLButtonElement | null;
-  const nextBtn = document.querySelector(
-    "#nextBtn"
-  ) as HTMLButtonElement | null;
+  const emblaNode = document.querySelector(".embla__viewport");
+  const prevBtn = document.querySelector("#prevBtn");
+  const nextBtn = document.querySelector("#nextBtn");
 
   if (!emblaNode || !prevBtn || !nextBtn) return;
 
@@ -21,12 +16,12 @@ export function initTestimoniosCarousel() {
   prevBtn.addEventListener("click", () => embla.scrollPrev());
   nextBtn.addEventListener("click", () => embla.scrollNext());
 
-  // Auto-play
   let autoPlayInterval = setInterval(() => embla.scrollNext(), 4000);
 
-  emblaNode.addEventListener("pointerenter", () =>
-    clearInterval(autoPlayInterval)
-  );
+  emblaNode.addEventListener("pointerenter", () => {
+    clearInterval(autoPlayInterval);
+  });
+
   emblaNode.addEventListener("pointerleave", () => {
     autoPlayInterval = setInterval(() => embla.scrollNext(), 4000);
   });
